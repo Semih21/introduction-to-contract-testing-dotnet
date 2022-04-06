82c35afde7e17d76e9034db4ebed9b684072137d
Bas Dijkstra <bas@ontestautomation.com>
2022-04-06 11:54:37 +0200

    Add address provider implementation

---
+++ AddressProvider/Controllers/AddressController.cs
@@ -0,0 +1,44 @@
+﻿using AddressProvider.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AddressProvider.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AddressController : ControllerBase
+    {
+        private readonly ILogger<AddressController> _logger;
+
+        public AddressController(ILogger<AddressController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("/address/{addressId}")]
+        public ActionResult<Address> GetAddressForId(string addressId)
+        {
+            if (addressId.ToLower().Equals("this_is_not_a_valid_address_id"))
+            {
+                return BadRequest();
+            }
+
+            if (addressId.ToLower().Equals("00000000-0000-0000-0000-000000000000"))
+            {
+                return NotFound();
+            }
+
+            return new Address
+            {
+                Id = Guid.Parse(addressId),
+                AddressType = "billing",
+                Street = "Main Street",
+                Number = 123,
+                City = "Nothingville",
+                ZipCode = 54321,
+                State = "Tennessee",
+                Country = "United States"
+            };
+        }
+    }
+}
---
+++ AddressProvider/Models/Address.cs
@@ -0,0 +1,14 @@
+﻿namespace AddressProvider.Models
+{
+    public class Address
+    {
+        public Guid Id { get; set; }
+        public string AddressType { get; set; }
+        public string Street { get; set; }
+        public int Number { get; set; }
+        public string City { get; set; }
+        public int ZipCode { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+    }
+}
